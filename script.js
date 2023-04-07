dc4278dd86bccc7b4a3cb7af350a3aa1f9c87405
Suraj Kumar <sahsuraj2001@gmail.com>
2023-04-07 17:32:09 +0530

    add servie page

--- script.js
+++ script.js
@@ -1,61 +1,66 @@
 const form = document.querySelector('.formClick');
+// let nameEl = document.getElementById('name');
+// let emailEl = document.getElementById('email');
+// let phoneEl = document.getElementById('phone');
+// let msg = document.getElementById('message');
 
 
+
+// Add an event listener for form submission
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let nameEl = document.getElementById('name').value;
-    let emailEl = document.getElementById('email').value;
-    let phoneEl = document.getElementById('phone').value;
-    let msg = document.getElementById('message').value;
-    
-    const pattern = /^[6789][0-9]{9}$/;
-    const emailValid = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  // Prevent the default form submission
+  e.preventDefault();
 
-  
+  // Get the form data
+  const formData = new FormData(form);
 
-    // Validate name
-    if (nameEl == "") {
-      document.querySelector("#name + .errorMsg").innerHTML = "Please enter your name";
-      document.getElementById("name").classList.add("is-invalid");
-    //   return false;
-    } else {
-    //   document.querySelector(".errorMsg").innerHTML = "";
-      document.getElementById("name").classList.remove("is-invalid");
-    }
+  let nameEl = document.getElementById('name').value.trim();
+  let emailEl = document.getElementById('email').value.trim();
+  let phoneEl = document.getElementById('phone').value.trim();
+  let msg = document.getElementById('message').value.trim();
 
-    // Validate email
-    if (!emailValid.test(emailEl)) {
-      document.querySelector("#email + .errorMsg").innerHTML = "Please enter your email";
-      document.getElementById("email").classList.add("is-invalid");
-    //   return false;
-    } else {
-    //   document.querySelector(".errorMsg").innerHTML = "";
-      document.getElementById("email").classList.remove("is-invalid");
-    }
+  const pattern = /^[6789][0-9]{9}$/;
+  const emailValid = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
-    // Validate phone
-    if (!pattern.test(phoneEl)) {
-      document.querySelector("#phone + .errorMsg").innerHTML = "Please enter valid phone number";
-      document.getElementById("phone").classList.add("is-invalid");
-    //   return false;
-    } else {
-    //   document.querySelector(".errorMsg").innerHTML = "";
-      document.getElementById("phone").classList.remove("is-invalid");
-    }
 
-    // Validate message
-    if (msg == "") {
-      document.querySelector("#message + .errorMsg").innerHTML = "Please enter a message";
-      document.getElementById("message").classList.add("is-invalid");
-    //   return false;
-    } else {
-    //   document.querySelector(".errorMsg").innerHTML = "";
-      document.getElementById("message").classList.remove("is-invalid");
-    }
-  
-    // form[0].reset();
+
+  // Validate name
+  if (nameEl == "") {
+    document.getElementById("name").classList.add("is-invalid");
+    return false;
+  } else {
+    document.getElementById("name").classList.remove("is-invalid");
+  }
+
+  // Validate email
+  if (!emailValid.test(emailEl)) {
+    document.getElementById("email").classList.add("is-invalid");
+    return false;
+  } else {
+    document.getElementById("email").classList.remove("is-invalid");
+  }
+
+  // Validate phone
+  if (!pattern.test(phoneEl)) {
+    document.getElementById("phone").classList.add("is-invalid");
+    return false;
+  } else {
+    document.getElementById("phone").classList.remove("is-invalid");
+  }
+
+  // Validate message
+  if (msg == "") {
+    document.getElementById("message").classList.add("is-invalid");
+    return false;
+  } else {
+    document.getElementById("message").classList.remove("is-invalid");
+  }
+
+
+  // Display an alert if the validation is successful
+  alert('Form Submited successful!');
+
+  form.reset();
 })
 
-
-
-
+ 
